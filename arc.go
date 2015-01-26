f2e65b7a9396c3276a96a45c1ed71a6ddec1e82c
Sonia Keys <skeys.cfa.harvard.edu>
2015-01-26 18:50:18 +0000

    rewrite ReadLine code with Scan, move channel logic to digest2

--- arc.go
+++ arc.go
@@ -5,100 +5,69 @@ package mpcformat
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"io"
 
 	"github.com/soniakeys/observation"
 )
 
-// Split splits an observation stream by designation, yielding observation arcs.
+// ArcSplitter returns a function that splits an observation stream by
+// designation, yielding parsed observation arcs.
 //
-// The stream iObs is a stream of observations in the MPC 80 column format.
-// The stream must have observations already grouped by designation and sorted
-// chronologically within each object.  That is, this function does not sort
-// them, but logic within the function relies on them being already sorted.
+// The stream rObs is a stream of observations in the MPC 80 column format.
+// The stream should have observations already grouped by designation.
+// That is, this function does not sort or accumulate groups, but simply
+// breaks the input stream at designation changes.
 //
-// Valid arcs are parsed against ocdMap and retuned on channel arcCh.
+// Observations are parsed against ocdMap.
 // Read errors are relayed on errCh should be considered fatal.
-// Parse errors are not fatal.  They are quietly ignored and not relayed
-// on errCh.  Lines causing parse errors and lines not forming valid arcs
-// are dropped without notification.
-func Split(iObs io.Reader, ocdMap observation.ParallaxMap,
-	arcCh chan *observation.Arc, errCh chan error) {
-	bf := bufio.NewReader(iObs)
-	var des0 string
-	var o observation.VObs
-	var desig string
-	obuf := make([]observation.VObs, 0, 4)
-	for {
-		bLine, pre, err := bf.ReadLine()
-		if err == io.EOF {
-			break
+// Parse errors are not fatal.
+func ArcSplitter(rObs io.Reader, ocdMap observation.ParallaxMap) func() (arc *observation.Arc, readError, formatError error) {
+	s := bufio.NewScanner(rObs)
+	var desig string       // designation last read
+	var o observation.VObs // observation last read
+	var a observation.Arc  // arc under construction
+	return func() (arc *observation.Arc, readError, parseError error) {
+		a.Obs = a.Obs[:0]
+		// o != nil means a valid observation was scanned on the previous call.
+		if o != nil {
+			a.Obs = append(a.Obs, o)
+			a.Desig = desig
 		}
-		if err != nil {
-			errCh <- err
-			break
-		}
-		if pre {
-			errCh <- errors.New("Split: unexpected long line")
-			break
-		}
-		if len(bLine) != 80 {
-			continue
-		}
-		line := string(bLine)
-		if line[14] == 's' {
-			if s, ok := o.(*observation.SatObs); ok {
-				ParseSat2(line, desig, s)
+		for {
+			if !s.Scan() {
+				return &a, io.EOF, nil
 			}
-			continue
+			err := s.Err()
+			if err != nil {
+				return &a, err, nil
+			}
+			line := s.Text()
+			if len(line) != 80 {
+				return &a, nil, fmt.Errorf(
+					"observation line length = %d, want 80", len(line))
+			}
+			if line[14] == 's' {
+				s, ok := o.(*observation.SatObs)
+				if !ok {
+					return &a, nil, errors.New(
+						"space-based observation line 2 without line 1")
+				}
+				if err := ParseSat2(line, desig, s); err != nil {
+					return &a, nil, err
+				}
+			}
+			switch desig, o, err = ParseObs80(line, ocdMap); {
+			case err != nil:
+				return &a, nil, err // return the parse error
+			case len(a.Obs) == 0:
+				a.Desig = desig // begin new arc
+				fallthrough
+			case desig == a.Desig:
+				a.Obs = append(a.Obs, o) // add observation to arc
+			default:
+				return &a, nil, nil // normal return
+			}
 		}
-		desig, o, err = ParseObs80(line, ocdMap)
-		switch {
-		case err != nil:
-			sendValid(des0, obuf, arcCh)
-			obuf = obuf[:0]
-		default:
-			sendValid(des0, obuf, arcCh)
-			fallthrough
-		case len(obuf) == 0:
-			des0 = desig
-			obuf = obuf[:1]
-			obuf[0] = o
-		case desig == des0:
-			obuf = append(obuf, o)
-		}
-	}
-	sendValid(des0, obuf, arcCh)
-	close(arcCh)
-}
-
-// checks that observations make a valid arc, allocates and sends.
-func sendValid(
-	desig string,
-	obuf []observation.VObs,
-	arcCh chan *observation.Arc,
-) {
-	if len(obuf) < 2 {
-		return
-	}
-	// the first observation time must be positive and
-	// observation times must increase after that
-	var t0 float64
-	for i := range obuf {
-		t := obuf[i].Meas().MJD
-		if t <= t0 {
-			return
-		}
-		t0 = t
-	}
-	// object must show motion over the arc
-	first := obuf[0].Meas()
-	last := obuf[len(obuf)-1].Meas()
-	if first.RA == last.RA && first.Dec == last.Dec {
-		return
-	}
-	arcCh <- &observation.Arc{
-		Desig: desig,
-		Obs:   append([]observation.VObs{}, obuf...),
 	}
 }
