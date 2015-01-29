6ff64c3acb8643079af24a4bd7f1a21d55ba6033
Sonia Keys <skeys.cfa.harvard.edu>
2015-01-29 15:33:30 +0000

    precompute mjd

--- tracklet_ex_test.go
+++ tracklet_ex_test.go
@@ -9,51 +9,51 @@ import (
 // an observation type for the example, with just enough data to
 // satisfy TrackletSplitter.
 type mock struct {
-	date string
 	site string
+	date string
+	mjd  float64
+}
+
+func mustMock(date, site string) mock {
+	mjd, ok := mpcformat.ParseObs80Date(date)
+	if !ok {
+		panic(site + " " + date)
+	}
+	return mock{site, date, mjd}
 }
 
 // implement TrackletSplitter
-func (m mock) MJD() float64 {
-	mjd, ok := mpcformat.ParseObs80Date(m.date)
-	if !ok {
-		panic(m.date)
-	}
-	return mjd
-}
-
-func (m mock) Observer() string {
-	return m.site
-}
+func (m mock) MJD() float64     { return m.mjd }
+func (m mock) Observer() string { return m.site }
 
 func ExampleFindTrackletsIndex() {
 	// (example data taken from MPEC-B144)
 	arc := []mpcformat.TrackletSplitter{
-		mock{"2015 01 23.39252", "F51"},
-		mock{"2015 01 23.40581", "F51"},
-		mock{"2015 01 23.41924", "F51"},
-		mock{"2015 01 23.43258", "F51"},
-		mock{"2015 01 24.218862", "H21"},
-		mock{"2015 01 24.220440", "H01"},
-		mock{"2015 01 24.224126", "H01"},
-		mock{"2015 01 24.224238", "H21"},
-		mock{"2015 01 24.22465", "H36"},
-		mock{"2015 01 24.230395", "H21"},
-		mock{"2015 01 24.234852", "H01"},
-		mock{"2015 01 24.243247", "H01"},
-		mock{"2015 01 24.24584", "H36"},
-		mock{"2015 01 25.16764", "807"},
-		mock{"2015 01 25.168554", "H01"},
-		mock{"2015 01 25.171843", "H01"},
-		mock{"2015 01 25.17513", "807"},
-		mock{"2015 01 25.18295", "807"},
-		mock{"2015 01 25.202440", "H01"},
-		mock{"2015 01 25.212352", "H01"},
-		mock{"2015 01 25.38900", "F51"},
-		mock{"2015 01 25.40205", "F51"},
-		mock{"2015 01 25.41513", "F51"},
-		mock{"2015 01 27.17787", "807"},
-		mock{"2015 01 27.18402", "807"},
+		mustMock("2015 01 23.39252", "F51"),
+		mustMock("2015 01 23.40581", "F51"),
+		mustMock("2015 01 23.41924", "F51"),
+		mustMock("2015 01 23.43258", "F51"),
+		mustMock("2015 01 24.218862", "H21"),
+		mustMock("2015 01 24.220440", "H01"),
+		mustMock("2015 01 24.224126", "H01"),
+		mustMock("2015 01 24.224238", "H21"),
+		mustMock("2015 01 24.22465", "H36"),
+		mustMock("2015 01 24.230395", "H21"),
+		mustMock("2015 01 24.234852", "H01"),
+		mustMock("2015 01 24.243247", "H01"),
+		mustMock("2015 01 24.24584", "H36"),
+		mustMock("2015 01 25.16764", "807"),
+		mustMock("2015 01 25.168554", "H01"),
+		mustMock("2015 01 25.171843", "H01"),
+		mustMock("2015 01 25.17513", "807"),
+		mustMock("2015 01 25.18295", "807"),
+		mustMock("2015 01 25.202440", "H01"),
+		mustMock("2015 01 25.212352", "H01"),
+		mustMock("2015 01 25.38900", "F51"),
+		mustMock("2015 01 25.40205", "F51"),
+		mustMock("2015 01 25.41513", "F51"),
+		mustMock("2015 01 27.17787", "807"),
+		mustMock("2015 01 27.18402", "807"),
 	}
 	for _, tk := range mpcformat.FindTrackletsIndex(arc) {
 		fmt.Println("")
