07b3b8af09d8dfe180609097c4162ef77286f4af
Sonia Keys <skeys.cfa.harvard.edu>
2015-01-30 21:37:04 +0000

    wip

--- arc.go
+++ arc.go
@@ -19,54 +19,62 @@ import (
 // That is, this function does not sort or accumulate groups, but simply
 // breaks the input stream at designation changes.
 //
-// Observations are parsed against ocdMap.
-// Read errors are relayed on errCh should be considered fatal.
-// Parse errors are not fatal.
-func ArcSplitter(rObs io.Reader, ocdMap observation.ParallaxMap) func() (arc *observation.Arc, readError, formatError error) {
+// Observations are parsed against pMap.
+// Read errors should be considered fatal.
+// Parse errors are not fatal but do terminate arcs.
+func ArcSplitter(rObs io.Reader, pMap observation.ParallaxMap) func() (*observation.Arc, error, error) {
 	s := bufio.NewScanner(rObs)
 	var desig string       // designation last read
 	var o observation.VObs // observation last read
 	var a observation.Arc  // arc under construction
-	return func() (arc *observation.Arc, readError, parseError error) {
+	return func() (arc *observation.Arc, readErr, parseErr error) {
 		a.Obs = a.Obs[:0]
+		arc = &a
 		// o != nil means a valid observation was scanned on the previous call.
 		if o != nil {
 			a.Obs = append(a.Obs, o)
 			a.Desig = desig
+			o = nil
 		}
 		for {
 			if !s.Scan() {
-				return &a, io.EOF, nil
-			}
-			err := s.Err()
-			if err != nil {
-				return &a, err, nil
+				if readErr = s.Err(); readErr != nil {
+					return
+				}
+				readErr = io.EOF
 			}
 			line := s.Text()
-			if len(line) != 80 {
-				return &a, nil, fmt.Errorf(
+			switch {
+			case len(line) == 80:
+			case len(line) == 0 && readErr == io.EOF:
+				o = nil
+				return
+			default:
+				parseErr = fmt.Errorf(
 					"observation line length = %d, want 80", len(line))
+				return
 			}
 			if line[14] == 's' {
 				s, ok := o.(*observation.SatObs)
 				if !ok {
-					return &a, nil, errors.New(
+					parseErr = errors.New(
 						"space-based observation line 2 without line 1")
+					return
 				}
-				if err := ParseSat2(line, desig, s); err != nil {
-					return &a, nil, err
+				if parseErr = ParseSat2(line, desig, s); parseErr != nil {
+					return
 				}
 			}
-			switch desig, o, err = ParseObs80(line, ocdMap); {
-			case err != nil:
-				return &a, nil, err // return the parse error
+			switch desig, o, parseErr = ParseObs80(line, pMap); {
+			case parseErr != nil:
+				return
 			case len(a.Obs) == 0:
 				a.Desig = desig // begin new arc
 				fallthrough
 			case desig == a.Desig:
 				a.Obs = append(a.Obs, o) // add observation to arc
 			default:
-				return &a, nil, nil // normal return
+				return // normal return
 			}
 		}
 	}
